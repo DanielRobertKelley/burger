ad6745d5d7142d0058cfd5520ff46aa896eb8b66
Daniel Kelley <danielrobertkelley@gmail.com>
2018-08-04 20:02:33 -0400

    burger

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Chesseburger",False);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Chesseburger",False);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Asian burger",False);
